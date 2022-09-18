2fe6a877d0c19b2f89a40021bf7161419ac29616
Chip135 <ethandchipman@gmail.com>
2022-09-17 19:03:23 -0600

    week 1 commit

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+
+document.getElementById("copyrightsymbol").innerHTML = `&copy`;
+
+document.getElementById("lastupdated").textContent = document.lastModified;
+
+document.getElementById("currentyear").innerHTML = d.getFullYear();
