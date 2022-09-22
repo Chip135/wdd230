604f0cad6062a6224787756eed248a1043be7bb8
Chip135 <ethandchipman@gmail.com>
2022-09-22 15:42:59 -0600

    Week 1 resubmit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
+const d = new Date();
+const year = d.getFullYear();
 
 document.getElementById("copyrightsymbol").innerHTML = `&copy`;
 
 document.getElementById("lastupdated").textContent = document.lastModified;
 
-document.getElementById("currentyear").innerHTML = d.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
