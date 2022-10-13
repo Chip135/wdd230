98341a3eb73b08c4a262a1e86bf90333426e6a87
Chip135 <ethandchipman@gmail.com>
2022-10-13 10:25:38 -0600

    bom commit

---
+++ lesson5/scripts/scripts.js
@@ -0,0 +1,22 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener("click", function () {
+	const myBook = input.value;
+	input.value = "";
+	const listItem = document.createElement("li");
+	const listText = document.createElement("span");
+	const listBtn = document.createElement("button");
+
+	listItem.appendChild(listText);
+	listText.textContent = myBook;
+	listItem.appendChild(listBtn);
+	listBtn.textContent = "❌";
+	list.appendChild(listItem);
+
+	listBtn.addEventListener("click", () => {
+		list.removeChild(listItem);
+	});
+	input.focus();
+});
