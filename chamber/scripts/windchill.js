2d42f8d1ea479d07de06c7f3c960a9508e0d3d51
Chip135 <ethandchipman@gmail.com>
2022-10-21 19:22:01 -0600

    wind chill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,26 @@
+const t = document.querySelector(".temp");
+const s = document.querySelector(".windspeed");
+const temp = parseInt(t.textContent);
+const speed = parseInt(s.textContent);
+console.log(temp);
+console.log(speed);
+
+function windchill(speed, temp){
+
+    let feeltemp = document.querySelector(".windchill");
+        
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+
+    wc = Math.floor(wc);
+
+    wc = (wc > temp) ? temp : wc;
+
+    console.log(wc);
+        
+    feeltemp.innerHTML = wc;
+
+    
+}
+
+windchill(speed, temp);
