20ac0a0d651be39e3135640bb8449921c46f51ce
Chip135 <ethandchipman@gmail.com>
2022-10-21 19:28:21 -0600

    added if statment

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,8 +6,8 @@ console.log(temp);
 console.log(speed);
 
 function windchill(speed, temp){
-
-    let feeltemp = document.querySelector(".windchill");
+    if (temp <= 50 && speed > 3){
+    const feeltemp = document.querySelector(".windchill");
         
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
@@ -21,6 +21,9 @@ function windchill(speed, temp){
     feeltemp.innerHTML = wc;
 
     
-}
+}else{
+    const feeltemp = document.querySelector(".windchill");
+    feeltemp.innerHTML = 'N/A';
+}}
 
 windchill(speed, temp);
