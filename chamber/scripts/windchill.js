d59aecb84b416167ed7d1b1893c7c445008d8b0b
Chip135 <ethandchipman@gmail.com>
2022-11-17 19:06:28 -0700

    wind chill fix

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -24,7 +24,6 @@ async function apiFetch() {
 
 function windchill(speed, temp){
     if (temp <= 50 && speed > 3){
-    const feeltemp = document.querySelector(".windchill");
         
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(temp);
@@ -38,8 +37,8 @@ function windchill(speed, temp){
 
     
 }else{
-    const feeltemp = document.querySelector(".windchill");
-    feeltemp.innerHTML = 'N/A';
+    wc = 'N/A';
+    return wc
 }}
 
 function capitalizeFirstLetter(str){
